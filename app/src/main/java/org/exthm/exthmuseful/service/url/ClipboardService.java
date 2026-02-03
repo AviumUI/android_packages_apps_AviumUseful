fb3cb16f1905246b8bdafc27f6c6ca2dc6a68b62
uwu-gl <moegluwu0@gmail.com>
2026-02-03 23:51:10 +0800

    ExthmUseful: Rewrite UI
    Change-Id: Ib1a575741c8ca8400a8c81dc27c59790564867c9

--- app/src/main/java/org/exthm/exthmuseful/service/url/ClipboardService.java
+++ app/src/main/java/org/exthm/exthmuseful/service/url/ClipboardService.java
@@ -37,7 +37,7 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-import org.exthm.exthmuseful.MainActivity;
+import org.exthm.exthmuseful.UsefulSettingsActivity;
 import org.exthm.exthmuseful.R;
 import org.exthm.exthmuseful.common.FloatingIconClickListener;
 import org.exthm.exthmuseful.common.FloatingIconManager;
@@ -190,7 +190,7 @@ public class ClipboardService extends Service implements ClipboardListener.OnCli
     }
 
     private Notification createNotification(String contentText) {
-        Intent notificationIntent = new Intent(this, MainActivity.class);
+        Intent notificationIntent = new Intent(this, UsefulSettingsActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
                 PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
 
